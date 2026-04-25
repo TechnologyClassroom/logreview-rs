99b8267ed64f7ca10ed40a8d4ddaaf05bdaa3720
TechnologyClassroom <michael@gnu.org>
2026-04-25 15:19:49 -0400

    Add date and merge in previous log parsing attempt before TOML.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
+// logreview-rs - Analyze web server logs.
+// Copyright (C) 2026 Michael McMahon
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+use chrono::Local;
 use serde_derive::Deserialize;
+use std::collections::HashMap;
+use std::fs::File;
 use std::fs;
+use std::io::{self, BufRead};
 use std::path::Path;
 use toml;
 
@@ -25,12 +45,60 @@ fn display_results(config: LogConfig) {
     println!("Column index: {}", config.column_index);
 }
 
+fn top_n_frequent_entries<P>(filename: P, n: usize) -> io::Result<Vec<(String, usize)>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    let reader = io::BufReader::new(file);
+
+    let mut frequency_map = HashMap::new();
+
+    for line in reader.lines() {
+        let line = line?;
+        // Split the line by whitespace and collect the first column
+        if let Some(first_column) = line.split_whitespace().next() {
+            *frequency_map.entry(first_column.to_string()).or_insert(0) += 1;
+        }
+    }
+
+    // Collecting results and sorting them based on frequency
+    let mut entries: Vec<(String, usize)> = frequency_map.into_iter().collect();
+    entries.sort_by(|a, b| b.1.cmp(&a.1)); // Sort in descending order by frequency
+
+    // Return only the top N entries
+    Ok(entries.into_iter().take(n).collect())
+}
+
+fn timeisthemaster() {
+    let date = Local::now();
+    println!("\nCurrent date/time: {}", date.format("[%Y-%m-%d][%H:%M:%S]"));
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    timeisthemaster();
+
     // Load configuration from file
     let config = load_config(CONFIG_FILE_PATH)?;
 
+    println!("\nConfig file loaded successfully!");  // Debug
+
     // Display results
     display_results(config);
+
+    // Set the log to scan.
+    let filename = "log/test1.log"; // This works.
+    //let filename = config.filenames; // This does not work.
+
+    // Parse IPs.
+    let top_entries = top_n_frequent_entries(filename, 10)?;
+
+    println!("\nTop 10 most frequent entries in the first column:");
+    for (entry, count) in top_entries {
+        println!("{}: {}", entry, count);
+    }
+
+    timeisthemaster();
 
     Ok(())
 }
